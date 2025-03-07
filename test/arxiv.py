c4c1738921532b1a8a76cd0b9e4a89f81d9d3b94
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-03-07 10:36:34 +0000

    update: add arxiv api test and parser xml

--- test/arxiv.py
+++ test/arxiv.py
@@ -1,17 +1,31 @@
 import requests
+import re
 import xml.etree.ElementTree as ET
+import pprint
 
 base_url = "http://export.arxiv.org/api/query"
 query = "search_query=all:electron"
 response = requests.get(f"{base_url}?{query}")
-print(response.text)
 
 
-def expand_children(node: ET.Element):
-    for child in node:
-        print(child.tag, child.attrib, child.text)
-        expand_children(child)
+def xml_to_json(xml: ET.Element):
+    # Convert XML to JSON
+    def expand_children(node: ET.Element):
+        obj = {
+            "tag": node.tag.split("}")[1],
+            "tag_prefix": re.findall(r"{.*}", node.tag)[0][1:-1],
+            "text": node.text,
+            "children": [],
+        }
+        for child in node:
+            obj["children"] = obj.get("children", []) + [expand_children(child)]
+        return obj
+
+    return expand_children(xml)
 
 
 root = ET.fromstring(response.text)
-expand_children(root)
+json = xml_to_json(root)
+
+
+pprint.pprint(json)
