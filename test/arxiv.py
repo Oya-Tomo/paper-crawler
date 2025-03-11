e60052670771be4caffd5ea4f18d72ac6a4b92fd
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-03-11 14:34:41 +0000

    update: add json parse test for arxiv api

--- test/arxiv.py
+++ test/arxiv.py
@@ -1,7 +1,9 @@
+from dataclasses import dataclass
 import requests
 import re
 import xml.etree.ElementTree as ET
 import pprint
+import json
 
 base_url = "http://export.arxiv.org/api/query"
 query = "search_query=all:electron"
@@ -15,8 +17,10 @@ def xml_to_json(xml: ET.Element):
             "tag": node.tag.split("}")[1],
             "tag_prefix": re.findall(r"{.*}", node.tag)[0][1:-1],
             "text": node.text,
+            "attributes": node.attrib,
             "children": [],
         }
+
         for child in node:
             obj["children"] = obj.get("children", []) + [expand_children(child)]
         return obj
@@ -24,8 +28,68 @@ def xml_to_json(xml: ET.Element):
     return expand_children(xml)
 
 
-root = ET.fromstring(response.text)
-json = xml_to_json(root)
+def xml_to_arxiv_json(xml: ET.Element):
+    data = xml_to_json(xml)
+
+    if data["tag"] != "feed":
+        raise ValueError("Expected root tag to be 'feed'")
+
+    entries = []
+
+    for child in data["children"]:
+        if child["tag"] == "entry":
+            entry = {
+                "id": None,
+                "title": None,
+                "summary": None,
+                "updated": None,
+                "published": None,
+                "authors": [],
+                "pdf": None,
+                "comment": None,
+                "journal_ref": None,
+                "doi": None,
+                "primary_category": None,
+                "categories": [],
+            }
+            for entry_child in child["children"]:
+                if entry_child["tag"] == "id":
+                    entry["id"] = entry_child["text"]
+                elif entry_child["tag"] == "title":
+                    entry["title"] = entry_child["text"]
+                elif entry_child["tag"] == "summary":
+                    entry["summary"] = entry_child["text"]
+                elif entry_child["tag"] == "updated":
+                    entry["updated"] = entry_child["text"]
+                elif entry_child["tag"] == "published":
+                    entry["published"] = entry_child["text"]
+                elif entry_child["tag"] == "author":
+                    author = {
+                        "name": None,
+                        "affiliation": None,
+                    }
+                    for author_child in entry_child["children"]:
+                        if author_child["tag"] == "name":
+                            author["name"] = author_child["text"]
+                        elif author_child["tag"] == "affiliation":
+                            author["affiliation"] = author_child["text"]
+                    entry["authors"].append(author)
+                elif entry_child["tag"] == "link":
+                    if entry_child["attributes"].get("type", None) == "application/pdf":
+                        entry["pdf"] = entry_child["attributes"]["href"]
+                elif entry_child["tag"] == "comment":
+                    entry["comment"] = entry_child["text"]
+                elif entry_child["tag"] == "journal_ref":
+                    entry["journal_ref"] = entry_child["text"]
+                elif entry_child["tag"] == "doi":
+                    entry["doi"] = entry_child["text"]
+                elif entry_child["tag"] == "primary_category":
+                    entry["primary_category"] = entry_child["attributes"]["term"]
+                elif entry_child["tag"] == "category":
+                    entry["categories"].append(entry_child["attributes"]["term"])
+            entries.append(entry)
+    return entries
 
 
-pprint.pprint(json)
+# print(json.dumps(xml_to_json(ET.fromstring(response.text))))
+print(json.dumps(xml_to_arxiv_json(ET.fromstring(response.text))))
