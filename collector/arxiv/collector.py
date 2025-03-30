c3ef97af8f3bdac509a6d6693f6ba5e02e4bf34d
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-03-30 01:54:44 +0000

    update: add paper source

--- collector/arxiv/collector.py
+++ collector/arxiv/collector.py
@@ -7,11 +7,10 @@ from typing import Self
 
 from api import xml_to_arxiv_json, url_to_arxiv_id
 
-ID_PREFIX = "arxiv:"
-
 
 class ArxivPaper(BaseModel):
     id: str
+    src: str = "arxiv"
 
     title: str
     abstract: str
@@ -31,7 +30,7 @@ class ArxivPaper(BaseModel):
     @classmethod
     def from_dict(self, data: dict) -> Self:
         return ArxivPaper(
-            id=ID_PREFIX + url_to_arxiv_id(data["id"]),
+            id=url_to_arxiv_id(data["id"]),
             title=data["title"],
             abstract=data["summary"],
             authors=[author["name"] for author in data["authors"]],
@@ -48,9 +47,21 @@ class ArxivPaper(BaseModel):
 
 
 def collect_arxiv_papers(
-    query: str, start: int = 0, max_results: int = 10
+    search_query: str | None = None,
+    id_list: str | None = None,
+    start: int = 0,
+    max_results: int = 10,
 ) -> list[ArxivPaper]:
-    url = f"http://export.arxiv.org/api/query?search_query={query}&start={start}&max_results={max_results}"
+    base_url = f"http://export.arxiv.org/api/query?"
+    params = []
+    if search_query is not None:
+        params.append(f"search_query={search_query}")
+    if id_list is not None:
+        params.append(f"id_list={id_list}")
+    params.append(f"start={start}")
+    params.append(f"max_results={max_results}")
+    url = base_url + "&".join(params)
+
     response = requests.get(url)
     xml = ET.fromstring(response.text)
     data = xml_to_arxiv_json(xml)
@@ -62,7 +73,11 @@ if __name__ == "__main__":
     query = "all:electron"
     start = 0
     max_results = 10
-    papers = collect_arxiv_papers(query, start, max_results)
+    papers = collect_arxiv_papers(
+        search_query=query,
+        start=start,
+        max_results=max_results,
+    )
 
     for paper in papers:
         print(paper.model_dump_json(indent=2))
