2eeb9d3b2bd269c888e5d4404d55aac8b3f3e016
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-03-31 04:50:24 +0000

    update: fixed env error handling

--- summarizer/env.py
+++ summarizer/env.py
@@ -3,10 +3,10 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "")
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", None)
 if not OPENAI_API_KEY:
     raise ValueError("OPENAI_API_KEY environment variable is not set.")
 
-OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
 if not OPENAI_MODEL:
     raise ValueError("OPENAI_MODEL environment variable is not set.")
